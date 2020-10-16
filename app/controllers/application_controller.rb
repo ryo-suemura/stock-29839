07197dc9a9ea38130b28fa8cc6e80e4ff6bb5424
ryo-suemura <ryosuemura@gmail.com>
2020-10-16 14:59:47 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :authenticate_user! #ログイン画面へ遷移させる
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
   private
 
   # ユーザー認証
@@ -8,5 +10,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
+  end
+# nameカラムの保存を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
