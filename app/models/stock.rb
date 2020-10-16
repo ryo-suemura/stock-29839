86142c12640ec6cdd42c00672c302cb6d12e078f
ryo-suemura <ryosuemura@gmail.com>
2020-10-16 19:37:42 +0900

    バリデーションの設定

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,10 @@ class Stock < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :code
   belongs_to_active_hash :publisher
+
+  with_options presence: true do
+    validates :name, :author, :publish_day, :stock, :genre, :code_id, :publisher_id, :condition, presence: true
+    validates :code_id, :publisher_id, numericality: { other_than: 1 }
+  end
+  
 end
