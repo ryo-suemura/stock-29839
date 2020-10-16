45d10bf1864c8a80873be38ba8573890e3062aa1
ryo-suemura <ryosuemura@gmail.com>
2020-10-16 12:29:47 +0900

    usersテーブルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   private
 
   # ユーザー認証
-  def basic_auth 
+  def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password ==ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
