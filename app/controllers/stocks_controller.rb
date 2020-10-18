00a0f6c183518549f1ca954517c84a6d2e65b333
ryo-suemura <ryosuemura@gmail.com>
2020-10-19 00:33:25 +0900

    コントローラーの設定

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,4 +3,17 @@ class StocksController < ApplicationController
   def index
     @stocks = Stock.all
   end
+
+  def new 
+    @stock = Stock.new
+  end
+
+  def create
+    Stock.create(stock_params)
+  end
+
+  private
+  def stock_name
+    params.require(:stock).permit(:number, :name, :author, :stock, :genre_id, :code_id, :publisher_id).merge(user_id: current_user.id)
+  end
 end
