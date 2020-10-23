74ae72a44187a170f4235ff6059373f7ca31a40d
ryo-suemura <ryosuemura@gmail.com>
2020-10-23 12:50:00 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "stocks#index"
   resources :users, only: [:edit, :update]
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :stocks, only: [:index, :new, :create] do
     resources :purchases, only: [:new, :create]
     resources :orders, only: [:new, :create]
