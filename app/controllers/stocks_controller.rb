90c18fa7586a0d5f69c9f0019f65eb20bc623f82
ryo-suemura <ryosuemura@gmail.com>
2020-11-02 16:33:33 +0900

    コントローラーの設定

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -21,6 +21,16 @@ class StocksController < ApplicationController
     @stock = Stock.find(params[:id])
   end
 
+  def update
+    @stock = Stock.find(params[:id])
+    if @stock.update(stock_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+  end
+
   def search
     @stocks = Stock.search(params[:keyword])
   end
